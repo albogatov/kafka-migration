aec706859d2d9aee6c77fec982d9e0c4e62c549e
abogatov <abogatov@solanteq.com>
2023-05-27 21:25:53 +0300

    Edit source topic

--- start.sh
+++ start.sh
@@ -16,7 +16,8 @@ wait-for-url() {
 }
 
 docker-compose -p mongo-kafka up -d --force-recreate       
-docker exec -it mongo1 mongosh --eval "rs.initiate( {  _id: 'rs0', members: [ { _id: 0, host: 'mongo1:27017' }, {  _id: 1, host: 'mongo2:27017'}]});"
+#docker exec -it mongo1 mongosh --eval "rs.initiate( {  _id: 'rs0', members: [ { _id: 0, host: 'mongo1:27017' }, {  _id: 1, host: 'mongo2:27017'}]});"
+docker exec -it mongo1 mongosh --eval "rs.initiate( {  _id: 'rs0', members: [ { _id: 0, host: 'mongo1:27017' }]});"
 sleep 15
 docker exec -i mongo1 /bin/bash -c "cx /scratch_space/src/sourceTopic.json"
 docker exec -i mongo1 /bin/bash -c "cx /scratch_space/dst/destinationTopic.json"
